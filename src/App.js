d6f301166e051b5ee181fcfea2920b0e75bbb328
ashan255 <59787574+ashan255@users.noreply.github.com>
2021-08-10 06:24:01 +0530

    Created a basic react layout to start with

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import Todo from "./components/Todo";
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Intro to ReactJS" />
-      <Todo text="ReactJS Fullstack" />
-      <Todo text="Exploring Advance ReactJS" />
-    </div>
-  );
+  return ()
 }
 
 export default App;
