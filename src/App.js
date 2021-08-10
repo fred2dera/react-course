acf282875f22c813049668c445b492cd62880c88
ashan255 <59787574+ashan255@users.noreply.github.com>
2021-08-10 07:08:57 +0530

    Created routes for the pages|nailed it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import Todo from "./components/Todo";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
 
 function App() {
-  return ()
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,5 @@
+function AllMeetups(props) {
+  return <div exact={props.exact}>All Meetups</div>;
+}
+
+export default AllMeetups;
